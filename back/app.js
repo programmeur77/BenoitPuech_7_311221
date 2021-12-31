b343bc364f7848a351780ab8f91a076949d45093
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-31 18:43:18 +0100

    Added url prefixes for publications routes

--- back/app.js
+++ back/app.js
@@ -4,9 +4,10 @@ const router = express.Router();
 
 const db = require('./config/db');
 const userRoutes = require('./routes/user.routes');
+const publicationRoutes = require('./routes/publication.routes')
 
 app.use(express.json());
 
 app.use('/api/auth', userRoutes);
-
+app.use('/api/publications', publicationRoutes)
 module.exports = app;
