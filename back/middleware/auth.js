9f460aff5244818ac3b4eb596c7747b0d7f41bd4
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-31 18:44:24 +0100

    Added JWT auth middleware to protect publications routes

---
+++ back/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.decode(token, process.env.JWT_STRING);
+    const userId = decodedToken.userId;
+    req.auth = { userId };
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'Invalid user ID';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: 'Unauthorized request'
+    });
+  }
+};
