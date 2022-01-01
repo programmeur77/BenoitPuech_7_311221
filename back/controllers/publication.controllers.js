538e28c7969566e7c4861cea809ccc8abff575d9
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-01 09:01:04 +0100

    Added the possibility to add image without text or both image and text to a publication WITHOUT multer yet + added posts.imageUrl to getall controller

--- back/controllers/publication.controllers.js
+++ back/controllers/publication.controllers.js
@@ -2,7 +2,7 @@ const db = require('./../config/db');
 
 exports.getAllPublications = (req, res) => {
   const getAllPublicationsRequest =
-    'SELECT lastName, firstName, posts.postContent, posts.post_date FROM user JOIN posts on posts.userId = user.id';
+    'SELECT lastName, firstName, posts.postContent, posts.imageUrl, posts.post_date FROM user JOIN posts on posts.userId = user.id';
 
   db.query(getAllPublicationsRequest, (error, result) => {
     if (!error) return res.status(200).json(result);
@@ -11,9 +11,23 @@ exports.getAllPublications = (req, res) => {
 };
 exports.postPublication = (req, res) => {
   const { userId, postContent } = req.body;
-  const publicationPostRequest = `INSERT INTO posts VALUES (null, ${req.body.userId}, '${req.body.postContent}', null, NOW())`;
 
-  db.query(publicationPostRequest, (error, result) => {
+  var query = '';
+
+  if (req.body.postContent === '' && req.body.imageUrl === '') {
+    return res.status(204).json({ error: 'Empty content' });
+  }
+
+  if (req.body.postContent !== '' && req.body.imageUrl === null) {
+    query = `INSERT INTO posts (userId, postContent) VALUES (${req.body.userId}, "${req.body.postContent}")`;
+  } else if (req.body.postContent === '' && req.body.imageUrl !== '') {
+    query = `INSERT INTO posts (userId, imageUrl) VALUES ("${req.body.userId}", "${req.body.imageUrl}")`;
+  } else if (req.body.postContent !== '' && req.body.imageUrl !== '') {
+    query = `INSERT INTO posts (userId, postContent, imageUrl) VALUES(${req.body.userId}, "${req.body.postContent}", "${req.body.imageUrl}")`;
+  }
+
+  db.query(query, (error, result) => {
+    console.log('case 1');
     if (result) {
       return res.status(201).json({ message: 'Successfully posted' });
     } else {
