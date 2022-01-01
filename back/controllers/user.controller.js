78b766eb174725328dcbfa74a23893e38cc0a13a
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-01 22:27:57 +0100

    Added GET, PUT and DELETE routes for user + Implemented correspond controllers

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { restart } = require('nodemon');
 
 const db = require('./../config/db');
+const { deletePublication } = require('./publication.controller');
 
 exports.signup = (req, res) => {
   const { lastName, firstName, email, password } = req.body;
@@ -22,7 +24,6 @@ exports.signup = (req, res) => {
 
 exports.login = (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body.email);
   const selectUserQuery = `SELECT id, email, password FROM user WHERE email = '${req.body.email}'`;
 
   db.query(selectUserQuery, function (error, result, field) {
@@ -44,5 +45,63 @@ exports.login = (req, res) => {
         });
       })
       .catch((error) => res.status(500).error({ error }));
+  });
+};
+
+exports.getProfile = (req, res) => {
+  const getProfileQuery = `SELECT id, lastName, firstName, email, avatarUrl, signup_date FROM user WHERE id = ${req.params.id} AND active = 1`;
+
+  db.query(getProfileQuery, (error, result) => {
+    if (!error) {
+      return res.status(200).json(result);
+    } else {
+      return res.status(500).json(error);
+    }
+  });
+};
+
+exports.modifyProfile = (req, res) => {
+  const modifyProfileQuery = `UPDATE user SET lastName = "${req.body.lastName}", firstName = "${req.body.firstName}", avatarUrl = "${req.body.avatarUrl}" WHERE id = ${req.body.userId}`;
+  if (req.body.userId === req.auth.userId) {
+    db.query(modifyProfileQuery, (error, result) => {
+      if (!error) {
+        return res.status(201).json({ message: 'Sucessfully updated' });
+      } else {
+        return res.status(400).json({ error });
+      }
+    });
+  } else {
+    return res.status(401).json({ message: 'Unauthorized request.' });
+  }
+};
+
+exports.deleteProfile = (req, res) => {
+  const getAdminStatusQuery = `SELECT id, admin FROM user WHERE id = ${req.params.id}`;
+  const deleteProfileQuery = `UPDATE user SET active = 0 WHERE id = ${req.params.id}`;
+
+  db.query(getAdminStatusQuery, (error, result) => {
+    if (result[0].admin == 1) {
+      db.query(deleteProfileQuery, (error, result) => {
+        if (!error) {
+          return res
+            .status(200)
+            .json({ message: 'Sucessfully desactivated profile' });
+        } else {
+          return res.status(500).json(error);
+        }
+      });
+    } else if (result[0].id === req.auth.userId) {
+      db.query(deleteProfileQuery, (error, result) => {
+        if (!error) {
+          return res
+            .status(200)
+            .json({ message: 'Sucessfully desactivated profile' });
+        } else {
+          return res.status(500).json(error);
+        }
+      });
+    } else {
+      return res.status(401).json({ message: 'Unauthorized request' });
+    }
   });
 };
--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('./../controllers/user.controller');
+const auth = require('./../middleware/auth');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+router.get('/:id', auth, userCtrl.getProfile);
+router.put('/:id', auth, userCtrl.modifyProfile);
+router.delete('/:id', auth, userCtrl.deleteProfile);
 
 module.exports = router;
