f3243e95c92b0da01ad6d7006fd1d96108c8b807
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-01 21:08:47 +0100

    Added routes to PUT and DELETE posts

--- back/app.js
+++ back/app.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const db = require('./config/db');
 const userRoutes = require('./routes/user.routes');
-const publicationRoutes = require('./routes/publication.routes')
+const publicationRoutes = require('./routes/publication.routes');
+const commentRoutes = require('./routes/comment.routes');
 
 app.use(express.json());
 
 app.use('/api/auth', userRoutes);
-app.use('/api/publications', publicationRoutes)
+app.use('/api/publications', publicationRoutes);
+app.use('/api/comments', commentRoutes);
+
 module.exports = app;
--- back/routes/publication.routes.js
+++ back/routes/publication.routes.js
@@ -7,7 +7,7 @@ const auth = require('./../middleware/auth');
 router.get('/', auth, publicationCtrl.getAllPublications);
 router.post('/post', auth, publicationCtrl.postPublication);
 router.get('/:id', auth, publicationCtrl.getOnePublication);
-router.put('/:id', auth, publicationCtrl.updatePublication);
+router.put('/:id', auth, publicationCtrl.modifyPublication);
 router.delete('/:id', auth, publicationCtrl.deletePublication);
 
 module.exports = router;
