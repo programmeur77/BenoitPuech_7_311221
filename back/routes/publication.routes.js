f0bfa2fdeb57ca6d1f8b04fba047458a21d149c5
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-01 14:02:08 +0100

    Added routes to other CRUD functions regarding posts

--- back/routes/publication.routes.js
+++ back/routes/publication.routes.js
@@ -6,5 +6,8 @@ const auth = require('./../middleware/auth');
 
 router.get('/', auth, publicationCtrl.getAllPublications);
 router.post('/post', auth, publicationCtrl.postPublication);
+router.get('/:id', auth, publicationCtrl.getOnePublication);
+router.put('/:id', auth, publicationCtrl.updatePublication);
+router.delete('/:id', auth, publicationCtrl.deletePublication);
 
 module.exports = router;
