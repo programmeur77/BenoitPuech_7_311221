98780d05d29324cf23d231abb3ebd81d9c2c18f0
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-05 16:37:20 +0100

    Contains VueJS front end of the app

---
+++ front/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <header>
+      <img src="./assets/logo.png" alt="White logo" class="header__logo" />
+    </header>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
+header {
+  width: 100%;
+  height: 1%;
+  display: flex;
+  justify-content: center;
+  background: #003171;
+}
+
+.header__logo {
+  width: 200px;
+  height: 200px;
+}
+</style>
---
+++ front/src/components/LoginForm.vue
@@ -0,0 +1,205 @@
+<template>
+  <section>
+    <h1 class="section__title">
+      Bienvenue sur votre réseau social d'entreprise
+    </h1>
+    <form class="login" @submit="postData" method="post">
+      <div id="form__errorMsg"></div>
+
+      <div
+        class="form__identity"
+        id="formIdentity"
+        v-if="signupStatus === false"
+      >
+        <div class="form__names">
+          <label for="nom" class="form__legend">Nom :</label><br />
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Nom"
+            class="form__field form__field--larger"
+          />
+        </div>
+
+        <div class="form__name">
+          <label for="prenom" class="form__legend">Prénom :</label><br />
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="Prénom"
+            class="form__field form__field--larger"
+          />
+        </div>
+      </div>
+      <label for="email" class="form__legend">Email :</label>
+      <input
+        type="text"
+        name="email"
+        id="email"
+        placeholder="exemple@exemple.com"
+        class="form__field"
+        v-model="posts.email"
+      />
+      <div id="password_error"></div>
+
+      <label for="password" class="form__legend">Mot de passe :</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Mot de passe"
+        class="form__field"
+        v-model="posts.password"
+      />
+      <div id="password_error"></div>
+
+      <div class="form__text">
+        <p class="form__signup" v-if="signupStatus === true">
+          Vous n'avez pas encore de compte ?
+          <span class="form__link" @click="signupStatus = false"
+            >S'inscrire</span
+          >
+        </p>
+        <p class="form__signup" v-if="signupStatus === false">
+          Vous avez déjà un compte ?
+          <span class="form__link" @click="signupStatus = true"
+            >Se connecter</span
+          >
+        </p>
+      </div>
+      <div class="form__submition">
+        <button class="form__submit" id="submit" v-if="signupStatus === true">
+          Se connecter
+        </button>
+        <button class="form__submit" id="submit" v-else>S'inscrire</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      posts: {
+        email: '',
+        password: ''
+      },
+      signupStatus: true
+    };
+  },
+  methods: {
+    postData: function (event) {
+      event.preventDefault();
+      axios
+        .post('http://localhost:3000/api/user/login', this.posts)
+        .then((data) => data)
+        .then((dataReturned) => {
+          if (dataReturned.status == 200) {
+            console.log(dataReturned.data);
+          } else if (dataReturned.status == 404) {
+            console.log(dataReturned.message);
+          }
+        });
+    }
+  }
+};
+</script>
+
+<style>
+section {
+  width: 100%;
+  margin-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-family: 'poppins';
+  border-radius: 5px;
+}
+
+.section__title {
+  width: 100%;
+  text-align: center;
+}
+
+form {
+  width: 100%;
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.form__identity {
+  width: 30%;
+  margin-top: -30px;
+  margin-left: 28px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: -1rem;
+  justify-content: center;
+}
+
+.form__legend {
+  width: 100%;
+  text-align: center;
+}
+
+.form__field {
+  width: 24%;
+  padding: 10px;
+  margin-top: 5px;
+  margin-bottom: 20px;
+  border: none;
+  border-bottom: 1px solid #003171;
+  outline: none;
+  font-size: 1rem;
+  font-family: 'Poppins';
+}
+
+.form__field--larger {
+  width: 80%;
+}
+
+.form__signup {
+  margin-bottom: 20px;
+  font-size: 1rem;
+}
+
+.form__submition,
+.form__text {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.form__link {
+  text-decoration: underline;
+  color: #0000ee;
+}
+
+.form__link:hover,
+.form__submit:hover {
+  cursor: pointer;
+}
+
+.form__link:visited {
+  color: #551a8b;
+}
+
+.form__submit {
+  width: 21%;
+  padding: 15px;
+  border: none;
+  background: #003171;
+  color: white;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 1rem;
+  border-radius: 5px;
+}
+</style>
