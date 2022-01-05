ff98dffe4a2ec609374dd59002bd09c132a515f6
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-05 16:32:56 +0100

    Added authorizetion headers for CORS

--- back/app.js
+++ back/app.js
@@ -9,6 +9,19 @@ const commentRoutes = require('./routes/comment.routes');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
 app.use('/api/user', userRoutes);
 app.use('/api/publications', publicationRoutes);
 app.use('/api/comments', commentRoutes);
