6dc18696fad37b40729cd16bffd9e69fda04f655
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-06 00:01:23 +0100

    Moved header content from App.js to a component .vue file

---
+++ front/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <router-view>
+      <router-link to="/">
+        <img src="./../assets/logo.png" alt="White logo" class="header__logo" />
+      </router-link>
+    </router-view>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
+header {
+  width: 100%;
+  height: 1%;
+  display: flex;
+  justify-content: center;
+  background: #003171;
+}
+
+.header__logo {
+  width: 200px;
+  height: 200px;
+}
+</style>
