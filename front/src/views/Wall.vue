37428e4a7c17791488a3b5a88b2e8f10638a45ce
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-08 22:04:22 +0100

    Creation of the view to display Wall component

---
+++ front/src/views/Wall.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Wall />
+  </div>
+</template>
+
+<script>
+import Wall from './../components/Wall.vue';
+
+document.title = 'Publications | Groupomania';
+
+export default {
+  name: 'userWall',
+  components: {
+    Wall
+  }
+};
+</script>
