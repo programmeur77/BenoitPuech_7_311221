6b0ad8c1de48e2156c1965b870af4c3c53fcaaa0
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-12 10:40:05 +0100

    Update path of store import

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import store from './store/';
+import store from './store/store';
 
 createApp(App).use(router).use(store).mount('#app');
