3ca4b304da2d50f941d221b4f5e524ebdf9ba7e5
Benoît Puech <benoit.puech@yahoo.fr>
2022-01-19 11:11:38 +0100

    Add profile display and modification condition to display it accordingly

--- front/src/components/Profile.vue
+++ front/src/components/Profile.vue
@@ -1,5 +1,4 @@
 <template>
-  {{ userProfile }}
   <div class="user-profile" v-for="profile in userProfile" :key="profile.id">
     <h1 class="user-profile__title">
       Profil de {{ profile.firstName }} {{ profile.lastName }}
@@ -12,9 +11,25 @@
         "
         class="profile-picture__image"
       />
+      <p v-if="modifyProfile === true" class="text-link">Modifier</p>
     </div>
     <div class="user-profile__info">
-      <p class="user-profile__info-txt">
+      <input
+        type="text"
+        class="user-profile__info-modify"
+        name="firstName"
+        :value="profile.firstName"
+        v-if="modifyProfile === true"
+      />
+      <input
+        type="text"
+        class="user-profile__info-modify"
+        name="firstName"
+        :value="profile.lastName"
+        @change="setNewLastName"
+        v-if="modifyProfile === true"
+      />
+      <p class="user-profile__info-txt" v-else>
         {{ profile.firstName }} {{ profile.lastName }}
       </p>
 
@@ -23,6 +38,23 @@
       <h3>Informations d'inscription</h3>
       <p class="user-profile__info-txt">
         Inscrit depuis le : {{ profile.signup_date }}
+      </p>
+    </div>
+    <div class="user-profile__modify" v-if="profile.id == userId">
+      <p
+        @click="modifyProfile = true"
+        v-if="modifyProfile === false"
+        class="text-link"
+      >
+        Modifier le profil
+      </p>
+
+      <p
+        @click="modifyProfile = false"
+        v-if="modifyProfile === true"
+        class="text-link"
+      >
+        Envoyer
       </p>
     </div>
   </div>
@@ -40,10 +72,24 @@ if (sessionToken) {
 
 export default {
   name: 'Profile',
+  data() {
+    return {
+      userId: '',
+      modifyProfile: false,
+      modifiedUserFirstName: '',
+      modifiedUserLastName: ''
+    };
+  },
   beforeMount() {
     this.getProfile(this.$route.params.userId);
+    this.getCurrentUserId();
   },
   methods: {
+    setNewLastName: function () {},
+
+    getCurrentUserId: function () {
+      this.userId = userSessionData.userId;
+    },
     getProfile: function (userId) {
       this.$store.dispatch('getUserProfile', userId);
     },
@@ -61,3 +107,32 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.user-profile {
+  width: 90%;
+  margin: 10px auto;
+  padding: 10px 0;
+  background: rgba(138, 185, 241, 0.4);
+  text-align: center;
+  font-family: 'poppins';
+}
+
+.user-profile .user-profile__info .user-profile__info-modify {
+  margin-top: 10px;
+  margin-right: 10px;
+  padding: 10px;
+}
+
+.text-link {
+  width: 10%;
+  margin: 0 auto;
+  padding: 10px;
+  background-color: #003171;
+  color: white;
+}
+
+.text-link:hover {
+  cursor: pointer;
+}
+</style>
