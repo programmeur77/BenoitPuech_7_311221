f8587d8f2f19e9e4ce91851e2f599b8001c8ebf8
Benoît Puech <benoit.puech@yahoo.fr>
2022-02-08 04:34:55 +0100

    Link middleware so that avater gestion can be done

--- back/routes/publication.routes.js
+++ back/routes/publication.routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const publicationCtrl = require('../controllers/publication.controller');
 const auth = require('./../middleware/auth');
+const multer = require('./../middleware/multer-config');
 
 router.get('/', auth, publicationCtrl.getAllPublications);
-router.post('/post', auth, publicationCtrl.postPublication);
-router.get('/:id', auth, publicationCtrl.getOnePublication);
-router.put('/:id', auth, publicationCtrl.modifyPublication);
+router.post('/post', auth, multer, publicationCtrl.postPublication);
+router.get('/:id', auth, multer, publicationCtrl.getOnePublication);
+router.put('/:id', auth, multer, publicationCtrl.modifyPublication);
 router.delete('/:id', auth, publicationCtrl.deletePublication);
 
 module.exports = router;
