b475b86d73fb4fda71c47441ae30888f428ef129
Benoît Puech <benoit.puech@yahoo.fr>
2022-02-08 04:36:18 +0100

    Link multer-config middleware so that image gestion can be done for users

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const userCtrl = require('./../controllers/user.controller');
 const auth = require('./../middleware/auth');
+const multer = require('./../middleware/multer-config');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.get('/:id', auth, userCtrl.getProfile);
-router.put('/:id', auth, userCtrl.modifyProfile);
-router.delete('/:id', auth, userCtrl.deleteProfile);
+router.get('/:id', auth, multer, userCtrl.getProfile);
+router.put('/:id', auth, multer, userCtrl.modifyProfile);
+router.delete('/:id', auth, multer, userCtrl.deleteProfile);
 
 module.exports = router;
