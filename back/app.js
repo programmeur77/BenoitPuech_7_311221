aad92c84fa58dd2d80077b0083720a347cbaab3d
Benoît Puech <benoit.puech@yahoo.fr>
2022-02-09 12:56:11 +0100

    Modified added a slash before images/ to solve image display issue

--- back/app.js
+++ back/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/user', userRoutes);
-app.use('images/', express.static(path.join(__dirname, 'images')));
+app.use('/images/', express.static(path.join(__dirname, 'images')));
 app.use('/api/publications', publicationRoutes);
 app.use('/api/comments', commentRoutes);
 
